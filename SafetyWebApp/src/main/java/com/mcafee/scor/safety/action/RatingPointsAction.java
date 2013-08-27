8e94875bb6c05d0509f449d526e6dff41b180297
ankit <ankit5990@gmail.com>
2013-08-28 00:55:37 +0530

    added the dao call in action as well. will correct the backend dao later.

--- SafetyWebApp/src/main/java/com/mcafee/scor/safety/action/RatingPointsAction.java
+++ SafetyWebApp/src/main/java/com/mcafee/scor/safety/action/RatingPointsAction.java
@@ -1,14 +1,15 @@
 package com.mcafee.scor.safety.action;
 
-import java.util.HashMap;
 import java.util.Map;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 
+import com.mcafee.scor.safety.dao.processedData.ProcessedDataDao;
 import com.mcafee.scor.safety.model.Coordinates;
 import com.mcafee.scor.safety.model.Rating;
 
@@ -16,13 +17,22 @@ import com.mcafee.scor.safety.model.Rating;
 
 @Controller
 public class RatingPointsAction {
+
+	@Autowired
+	private ProcessedDataDao processedDataDao;
 	
 	@RequestMapping(value="/getRatingMapAroundCoordinate.do", method=RequestMethod.GET)
 	public @ResponseBody Map<Coordinates, Rating> getRatingOfRegionForCoordinate(@RequestParam("coordinateString") String coordinateString,
 			@RequestParam("radius") int radius){
-		Map<Coordinates, Rating> coordinateToRatingMap = new HashMap<Coordinates, Rating>();
-		coordinateToRatingMap.put(Coordinates.getFromString(coordinateString), Rating.GREEN);
-		coordinateToRatingMap.put(Coordinates.getFromString(coordinateString+"1"), Rating.RED);
-		return coordinateToRatingMap;
+		return getProcessedDataDao().getRatingAroundCoordinate(Coordinates.getFromString(coordinateString), radius);
 	}
+
+	public ProcessedDataDao getProcessedDataDao() {
+		return processedDataDao;
+	}
+
+	public void setProcessedDataDao(ProcessedDataDao processedDataDao) {
+		this.processedDataDao = processedDataDao;
+	}
+	
 }
