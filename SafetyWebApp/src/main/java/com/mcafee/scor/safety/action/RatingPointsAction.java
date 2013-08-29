91598f719ba5047cc629de237ba6ed9e171cce29
ankit <ankit5990@gmail.com>
2013-08-30 02:48:10 +0530

    added sample call for actions

--- SafetyWebApp/src/main/java/com/mcafee/scor/safety/action/RatingPointsAction.java
+++ SafetyWebApp/src/main/java/com/mcafee/scor/safety/action/RatingPointsAction.java
@@ -27,6 +27,17 @@ public class RatingPointsAction {
 	@Autowired
 	private UserRatingDao userRatingDao; 
 	
+	/**
+	 * 
+	 * sample url:
+	 * http://localhost:8080/SafetyWebApp/getRatingMapAroundCoordinate.do?coordinateString=1,1&timeMillies=2345656&transport=3&radius=2
+	 * 
+	 * @param coordinateString
+	 * @param timeMillies
+	 * @param transport
+	 * @param radius
+	 * @return
+	 */
 	@RequestMapping(value="/getRatingMapAroundCoordinate.do", method={RequestMethod.GET,RequestMethod.POST})
 	public @ResponseBody Map<Coordinates, Rating> getRatingOfRegionForCoordinate(@RequestParam("coordinateString") String coordinateString,
 			@RequestParam("timeMillies") long timeMillies,
@@ -37,6 +48,16 @@ public class RatingPointsAction {
 		return getProcessedDataDao().getRatingAroundCoordinate(Coordinates.getFromString(coordinateString), timeOfDay, transportEnum, radius);
 	}
 	
+	/**
+	 * sample url:
+	 * http://localhost:8080/SafetyWebApp/saveRating.do?coordinateString=1,1&timeMillies=223543463&transport=2&rating=GREEN
+	 * 
+	 * @param coordinateString
+	 * @param timeMillies
+	 * @param transport
+	 * @param rating
+	 * @return
+	 */
 	@RequestMapping(value="/saveRating.do",method={RequestMethod.GET,RequestMethod.POST})
 	public @ResponseBody int saveRating(@RequestParam("coordinateString") String coordinateString,
 			@RequestParam("timeMillies") long timeMillies,
